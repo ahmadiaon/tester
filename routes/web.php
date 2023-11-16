6774de64cad32c011d1d927c6da0df9bcf68065a
ahmadiaon <dataahmadi2020@gmail.com>
2023-11-16 15:10:24 +0700

    firtt

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+Route::get('/home', function () {
+    return view('home');
+});
+Route::get('/login', function () {
+    return view('login');
+});
+
+
+
